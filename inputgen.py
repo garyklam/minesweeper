b3c2ba59d9f743a68722763030d8978681a6e045
Gary Lam <garykailam@gmail.com>
2021-01-26 20:35:40 -0800

    handles fields with 0 as a dimension

--- inputgen.py
+++ inputgen.py
@@ -24,12 +24,13 @@ class InputGenerator:
     def _convert_field(self, parameters):
         x = int(parameters[0])
         y = int(parameters[1])
+        self.minefields += f'{x} {y}\n'
+        if x*y == 0:
+            return
         try:
             minechance = int(parameters[2])
         except IndexError:
             minechance = 10
-
-        self.minefields += f'{x} {y} \n'
         for i in range(0, x):
             for j in range(0, y):
                 input = choices((".","*"), (100-minechance, minechance))
@@ -49,6 +50,6 @@ class InputGenerator:
         input_file.write(self.minefields)
 
 if __name__ == '__main__':
-    test = InputGenerator((3,5), (4,4), (1, 10, 100), (5, 2, 0), (0,0))
+    test = InputGenerator((3,5), (4,4),(5, 0), (0, 3), (1, 10, 100), (5, 2, 0), (0, 0))
     print(test.print())
     test.create_input_file()
