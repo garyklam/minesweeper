fedbc4ade7876dd4142686ba93fbbf41a9de0d6b
Gary Lam <garykailam@gmail.com>
2021-01-26 21:05:56 -0800

    Added documentation

--- IndSolnGary.py
+++ IndSolnGary.py
@@ -1,12 +1,17 @@
 class Minefield:
+    """Holds the text representation of a single minefield based. Converts this to the desired numerical representation
+    when convert() is called. Returns the current state of the minefield with __str__."""
     def __init__(self, row, col, field_number):
+        self.string = ""
         self.field_num = field_number
         self.row = row
         self.col = col
+        self.string = f'Field #{self.field_num}:\n'
         self.field = [["." for col in range(col)] for row in range(row)]
         self.current_row = 0
 
     def add_row(self, string):
+        """Adds the string as a new row in the 2d array of mines."""
         col = 0
         for char in string:
             self.field[self.current_row][col] = char
@@ -14,13 +19,21 @@ class Minefield:
         self.current_row += 1
 
     def convert(self):
+        """Checks each cell in the 2d array, if the spot is not a mine, it calls _find_adjacent_mines to determine
+        how many mines are adjacent and inserts that number into the string. If the cell is a mine, inserts a *."""
         for row in range(self.row):
             for col in range(self.col):
                 if self.field[row][col] == ".":
                     mines = self._find_adjacent_mines(row, col)
-                    self.field[row][col] = mines
+                    self.string += f'{mines}'
+                else:
+                    self.string += f'*'
+            self.string += "\n"
 
     def _find_adjacent_mines(self, row, col):
+        """Determines the indices of the cells adjacent to the cell indicated by the parameters. Checks the if each
+        adjacent cell is within the bounds of the 2d array. If it is and the cell contains a mine, adds 1 to the mine
+        counter. Returns the number of mines once each adjacent cell is checked."""
         mines = 0
         y_range = range(row-1, row+2)
         x_range = range(col-1, col+2)
@@ -33,32 +46,35 @@ class Minefield:
         return mines
 
     def _check_bounds(self, y, x):
+        """Checks if the row and column are within the bounds of the 2d-array initialized by the constructor."""
         if self.row-1 >= y >= 0 and self.col-1 >= x >= 0:
             return True
  
     def __str__(self):
-        string = f'Field #{self.field_num}:\n'
-        for row in self.field:
-            for cell in row:
-                string += str(cell)
-            string += "\n"
-        return string
+        return self.string
 
 
 class Minesweeper:
-    def __init__(self):
-        self.field_num = 1
-        self.string_representation = ""
-        self.build_fields()
+    """Takes input from file redirect and builds a string containing the minefield information. Passes this info line
+    by line to a Minefield object that converts the field to the numerical representation and stores this representation
+    to be returned with a __str__ method."""
 
-    def build_fields(self):
-        mine_text = ""
+    def __init__(self):
+        """Field count starts at 1, initializes empty string to hold the converted text form. Starts while loop to
+        collect input line by line and stores it in an array called mine_text. Once a line containing 0 0, an end
+        marker is added to mine_text and the loop breaks. Another loop begins that takes the first line and obtains
+        field dimensions and creates a Minefield objects with those dimensions, it then passes the subsequent number
+        of lines to the Minefield based on the row dimension. The Minefield is converted and saved to this class before
+        moving to the next line, continuing until the end marker is reached."""
+        field_num = 1
+        self.string_representation = ""
+        mine_text = []
         while True:
             line = input()
             if line == "0 0":
+                mine_text.append("")
                 break
-            mine_text += f'{line}\n'
-        mine_text = mine_text.split("\n")
+            mine_text.append(f'{line}')
         curr_line = 0
         while True:
             line = mine_text[curr_line]
@@ -67,10 +83,10 @@ class Minesweeper:
             dimensions = line.split(" ")
             row, col = int(dimensions[0]), int(dimensions[1])
             if row*col > 0:
-                field = Minefield(row, col, self.field_num)
+                field = Minefield(row, col, field_num)
                 for i in range(1, int(dimensions[0])+1):
                     field.add_row(mine_text[curr_line+i])
-                self.field_num += 1
+                field_num += 1
                 field.convert()
                 self.string_representation += f'{field}\n'
                 curr_line += int(dimensions[0]) + 1
