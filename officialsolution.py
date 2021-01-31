8e941e1ebbee7cf359a596cea4b1edd987ad3efc
Gary Lam <garykailam@gmail.com>
2021-01-30 16:58:55 -0800

    Added more unit tests

--- officialsolution.py
+++ officialsolution.py
@@ -82,64 +82,107 @@ class Minesweeper:
 
 class UnitTests(unittest.TestCase):
 
-    def test_max_rows(self):
-        gen = InputGenerator((100, 1, 0))
-        gen.create_input_file()
-        expected = "Field #1\n"
-        for i in range(100):
-            expected += "0\n"
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
-        self.assertEqual(f'{minesweeper}', expected)
-
-    def test_max_columns(self):
-        gen = InputGenerator((1, 100, 0))
-        gen.create_input_file()
-        expected = "Field #1\n"
-        for i in range(100):
-            expected += "0"
-        expected += "\n"
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
-        self.assertEqual(f'{minesweeper}', expected)
-
-    def test_max_rows_and_columns(self):
-        gen = InputGenerator((100, 100, 0))
-        gen.create_input_file()
-        expected = "Field #1\n"
-        for i in range(100):
-            for j in range(100):
-                expected += "0"
-            expected += "\n"
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
-        self.assertEqual(f'{minesweeper}', expected)
-
-    def test_zero_rows(self):
-        gen = InputGenerator((0, 10))
-        gen.create_input_file()
-        expected = ""
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
-        self.assertEqual(f'{minesweeper}', expected)
-
-    def test_zero_cols(self):
-        gen = InputGenerator((10, 0))
-        gen.create_input_file()
-        expected = ""
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
-        self.assertEqual(f'{minesweeper}', expected)
-
     def test_all_mines(self):
         gen = InputGenerator((10, 5, 100))
-        gen.create_input_file()
+        gen.create_input_file(file_name="test_input.txt")
         expected = "Field #1\n"
         for i in range(10):
             for j in range(5):
                 expected += "*"
             expected += "\n"
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_no_mines(self):
+        gen = InputGenerator((5, 10, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = "Field #1\n"
+        for i in range(5):
+            for j in range(10):
+                expected += "0"
+            expected += "\n"
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_max_rows(self):
+        gen = InputGenerator((100, 5, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = "Field #1\n"
+        for i in range(100):
+            for j in range(5):
+                expected += "0"
+            expected += "\n"
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_max_columns(self):
+        gen = InputGenerator((5, 100, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = "Field #1\n"
+        for i in range(5):
+            for j in range(100):
+                expected += "0"
+            expected += "\n"
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_max_rows_and_columns(self):
+        gen = InputGenerator((100, 100, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = "Field #1\n"
+        for i in range(100):
+            for j in range(100):
+                expected += "0"
+            expected += "\n"
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_one_row(self):
+        gen = InputGenerator((1, 100, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = "Field #1\n"
+        for i in range(1):
+            for j in range(100):
+                expected += "0"
+            expected += "\n"
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_one_column(self):
+        gen = InputGenerator((100, 1, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = "Field #1\n"
+        for i in range(100):
+            for j in range(1):
+                expected += "0"
+            expected += "\n"
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_zero_rows(self):
+        gen = InputGenerator((0, 10))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = ""
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_zero_cols(self):
+        gen = InputGenerator((10, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = ""
+        minesweeper = Minesweeper(file="test_input.txt")
+        self.assertEqual(f'{minesweeper}', expected)
+
+    def test_zero_cols_and_zero_rows(self):
+        gen = InputGenerator((0, 0))
+        gen.create_input_file(file_name="test_input.txt")
+        expected = ""
+        minesweeper = Minesweeper(file="test_input.txt")
         self.assertEqual(f'{minesweeper}', expected)
 
     def test_empty_field_at_start(self):
         gen = InputGenerator((0, 5), (2, 4, 0), (4, 2, 100))
-        gen.create_input_file()
+        gen.create_input_file(file_name="test_input.txt")
         expected = "Field #1\n"
         for i in range(2):
             for j in range(4):
@@ -150,12 +193,12 @@ class UnitTests(unittest.TestCase):
             for j in range(2):
                 expected += "*"
             expected += "\n"
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
+        minesweeper = Minesweeper(file="test_input.txt")
         self.assertEqual(f'{minesweeper}', expected)
 
     def test_empty_field_in_middle(self):
         gen = InputGenerator((2, 4, 0), (0, 5), (4, 2, 100))
-        gen.create_input_file()
+        gen.create_input_file(file_name="test_input.txt")
         expected = "Field #1\n"
         for i in range(2):
             for j in range(4):
@@ -166,12 +209,12 @@ class UnitTests(unittest.TestCase):
             for j in range(2):
                 expected += "*"
             expected += "\n"
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
+        minesweeper = Minesweeper(file="test_input.txt")
         self.assertEqual(f'{minesweeper}', expected)
 
     def test_empty_field_at_end(self):
         gen = InputGenerator((2, 4, 0), (4, 2, 100), (0, 5))
-        gen.create_input_file()
+        gen.create_input_file(file_name="test_input.txt")
         expected = "Field #1\n"
         for i in range(2):
             for j in range(4):
@@ -182,7 +225,7 @@ class UnitTests(unittest.TestCase):
             for j in range(2):
                 expected += "*"
             expected += "\n"
-        minesweeper = Minesweeper(file="minesweeper_input.txt")
+        minesweeper = Minesweeper(file="test_input.txt")
         self.assertEqual(f'{minesweeper}', expected)
 
     def test_generic_two_by_two(self):
@@ -281,9 +324,8 @@ class UnitTests(unittest.TestCase):
         minesweeper = Minesweeper(manual_input=input)
         self.assertEqual(f'{minesweeper}', expected)
 
-    def test(self):
-        pass
-
 
 if __name__ == '__main__':
-    unittest.main()
+    test = Minesweeper()
+    print(test)
+    # unittest.main()
